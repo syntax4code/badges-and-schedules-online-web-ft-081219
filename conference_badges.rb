d6346bf246e723ca71cfdf19ce15c19825629d7c
Warren Loy Harper II <warrenharper2@att.net>
2019-08-15 17:51:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,12 @@
 def badge_maker(name)
- 
- 
-  "Hello, my name is _____."
-end
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
+  
+end 

fe24de69dbd84b2d0de2ca954b06eea47422e64f
Warren Loy Harper II <warrenharper2@att.net>
2019-08-15 17:53:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,12 @@ def batch_badge_creator(attendees)
   return badges
   
 end 
+
+def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
